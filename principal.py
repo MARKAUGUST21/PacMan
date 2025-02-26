f89299aa0021578a1b3b86cd4f2579ebb56d7625
Marcos Pedro Camargo <mcmpgui12@gmail.com>
2025-02-26 16:37:26 -0300

    Criando a tela do jogo
    Criando tela - organizando arquivos

---
+++ principal.py
@@ -0,0 +1,15 @@
+import pygame
+import constantes
+import sprites
+
+class Game:
+    def __init__(self):
+        # Criando uma tela para o jogo
+        pygame.init() 
+        pygame.mixer.init()  
+        self.tela = pygame.display.set_mode((constantes.LARGURA, constantes.ALTURA))
+        pygame.display.set_caption(constantes.TITULO_JOGO)
+        self.relogio = pygame.time.Clock()
+        self.esta_rodando = True
+    
+    def novo_jogo()
